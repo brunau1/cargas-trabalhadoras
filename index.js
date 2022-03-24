c830ae83a6fb9b2acc670b84a72f5586d6511457
brunau1 <brunosantosprotec@gmail.com>
2022-03-23 21:04:06 -0300

    feat: call analysis methods in index

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
-const DataHandler = require("./src/data-handler");
+const {
+  transformInputDataToIterableObject,
+  generateWorkLoadModelObject,
+} = require("./src/data-handler");
+const { showGeneratedWorkLoadModel } = require("./src/interface-handler");
+const {
+  calcAverage,
+  calcStandardDeviation,
+  calcVariationCoefficient,
+} = require("./src/math-handler");
 
-const workLoadInput = DataHandler.transformInputDataToIterableObject();
+const formatedInputData = transformInputDataToIterableObject();
+const workLoadModel = generateWorkLoadModelObject();
+
+// Calculo do modelo de carga de trabalho
+
+// NumProcs
+workLoadModel.numProcs.average = calcAverage(formatedInputData.numProcs);
+workLoadModel.numProcs.standardDeviation = calcStandardDeviation(
+  formatedInputData.numProcs
+);
+workLoadModel.numProcs.variationCoefficient = calcVariationCoefficient(
+  formatedInputData.numProcs
+);
+
+// ExeTime
+workLoadModel.exeTime.average = calcAverage(formatedInputData.exeTime);
+workLoadModel.exeTime.standardDeviation = calcStandardDeviation(
+  formatedInputData.exeTime
+);
+workLoadModel.exeTime.variationCoefficient = calcVariationCoefficient(
+  formatedInputData.exeTime
+);
+
+// InterArrivalTime
+workLoadModel.interArrivalTime.average = calcAverage(
+  formatedInputData.interArrivalTime
+);
+workLoadModel.interArrivalTime.standardDeviation = calcStandardDeviation(
+  formatedInputData.interArrivalTime
+);
+workLoadModel.interArrivalTime.variationCoefficient = calcVariationCoefficient(
+  formatedInputData.interArrivalTime
+);
+
+showGeneratedWorkLoadModel(workLoadModel);

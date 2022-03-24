c82e59ec5e078627db407f874c94454bef7df32f
brunau1 <brunosantosprotec@gmail.com>
2022-03-23 21:03:21 -0300

    feat: added prop to input and workload model

--- src/data-handler.js
+++ src/data-handler.js
@@ -1,35 +1,68 @@
 const Fs = require("fs");
 const Path = require("path");
 
+function transformInputDataToIterableObject() {
+  const filename = Path.resolve(__dirname, "../public/logSupercomputador.txt");
+  const inputString = Fs.readFileSync(filename, "utf8");
+  const sanitizedInputString = inputString
+    .replace(/\r/g, "")
+    .replace(/\t/g, ";");
+  const splitedStringArray = sanitizedInputString.split("\n");
+  splitedStringArray.shift();
+  const obj = {
+    subTime: [],
+    numProcs: [],
+    exeTime: [],
+    interArrivalTime: [],
+  };
+  let cont = 0; // contador para limitar o tamanho da base de dados
+  for (const line of splitedStringArray) {
+    if (cont < 500) {
+      const splitedLine = line.split(";");
+      obj.subTime.push(parseInt(splitedLine[0]));
+      obj.numProcs.push(parseInt(splitedLine[1]));
+      obj.exeTime.push(parseInt(splitedLine[2]));
+      cont++;
+    } else break;
+  }
+  obj.interArrivalTime = calcInterArrivalTime(obj);
+  const outDir = Path.resolve(__dirname, "../public/", "temp.json");
+  Fs.writeFileSync(outDir, JSON.stringify(obj, null, 2));
+  return obj;
+}
+
+function generateWorkLoadModelObject() {
+  return {
+    numProcs: {
+      average: 0,
+      standardDeviation: 0,
+      variationCoefficient: 0,
+    },
+    exeTime: {
+      average: 0,
+      standardDeviation: 0,
+      variationCoefficient: 0,
+    },
+    interArrivalTime: {
+      average: 0,
+      standardDeviation: 0,
+      variationCoefficient: 0,
+    },
+  };
+}
+
+function calcInterArrivalTime(obj) {
+  const interArrivalTime = [0];
+
+  for (let i = 1; i < obj.subTime.length; i++) {
+    const currentTime = obj.subTime[i];
+    const previousTime = obj.subTime[i - 1];
+    interArrivalTime.push(currentTime - previousTime);
+  }
+  return interArrivalTime;
+}
+
 module.exports = {
-  transformInputDataToIterableObject: () => {
-    const filename = Path.resolve(
-      __dirname,
-      "../public/logSupercomputador.txt"
-    );
-    const inputString = Fs.readFileSync(filename, "utf8");
-    const sanitizedInputString = inputString
-      .replace(/\r/g, "")
-      .replace(/\t/g, ";");
-    const splitedStringArray = sanitizedInputString.split("\n");
-    splitedStringArray.shift();
-    const obj = {
-      subTime: [],
-      numProcs: [],
-      exeTime: [],
-    };
-    let cont = 0; // contador para limitar o tamanho da base de dados
-    for (const line of splitedStringArray) {
-      if (cont < 10) {
-        const splitedLine = line.split(";");
-        obj.subTime.push(splitedLine[0]);
-        obj.numProcs.push(splitedLine[1]);
-        obj.exeTime.push(splitedLine[2]);
-        cont++;
-      } else break;
-    }
-    const outDir = Path.resolve(__dirname, "../public/", "temp.json");
-    Fs.writeFileSync(outDir, JSON.stringify(obj)); //JSON.stringify(obj, null, 2)
-    return obj;
-  },
+  transformInputDataToIterableObject,
+  generateWorkLoadModelObject,
 };
